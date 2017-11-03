981c4ff1fffc05814c6eefa4e31e648339950cb7
vagrant <vagrant@homestead>
2017-11-03 11:13:55 +0000

    Add account activations

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -32,8 +32,14 @@ class SessionController extends Controller
         ];
 
         if (Auth::attempt($credentials, $request->has('remember'))){
-            session()->flash('success','欢迎再次来到xxx世界');
-            return redirect()->intended(route('users.show', [Auth::user()]));
+            if(Auth::user()->activated){
+                session()->flash('success','欢迎再次来到xxx世界');
+                return redirect()->intended(route('users.show', [Auth::user()]));
+            }else{
+                Auth::logout();
+                session()->flash('warning','您的账户没有激活，请查看邮件。');
+                return redirect('/');
+            }
         }else{
             session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
             return redirect()->back();
