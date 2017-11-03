8936afdb010419ec0242d860d9af64220d11b81a
vagrant <vagrant@homestead>
2017-11-03 10:23:03 +0000

    Finish user edit, update, index, and destroy

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,23 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except' => ['show', 'create', 'store', 'index']
+        ]);
+
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
+    
     public function create()
     {
         return view('users.create');
@@ -37,18 +54,38 @@ class UsersController extends Controller
         return redirect()->route('users.show',[$user]);
     }
 
-    public function edit()
+    public function edit(User $user)
     {
-        return view('users.edit');
+        $this->authorize('update',$user);
+        return view('users.edit',compact('user'));
     }
 
-    public function update()
+    public function update(Request $request, User $user)
     {
-        return view('users.update');
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $this->authorize('update',$user);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if($request->password){
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success','更新资料成功');
+
+        return redirect()->route('users.show', $user->id);
     }
 
-    public function delete()
+    public function destroy(User $user)
     {
-        return view('users.delete');
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success','成功删除用户！');
+        return back();
     }
 }
